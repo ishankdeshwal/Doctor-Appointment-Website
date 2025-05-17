c512ead90b7a1d474a2d26d71171f6f81e56d678
Ishank <deshwalishank@gmail.com>
2025-05-18 01:43:29 +0530

    dm

--- Backend/Controllers/adminController.js
+++ Backend/Controllers/adminController.js
@@ -1,7 +1,7 @@
 import validator from "validator";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
-import doctorModel from "../models/doctorModel.js";
+import doctorModel from "../Models/doctorModel.js";
 import jwt from "jsonwebtoken";
 import appointmentModel from "../Models/AppointmentModel.js";
 import userModel from "../Models/UserModel.js";
