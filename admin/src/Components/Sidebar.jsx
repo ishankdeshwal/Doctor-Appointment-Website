0779b32d809a20bd5e0d52c2bb3865ac6ed13c42
Ishank <deshwalishank@gmail.com>
2025-05-18 03:06:13 +0530

    sidebar

--- admin/src/Components/Sidebar.jsx
+++ admin/src/Components/Sidebar.jsx
@@ -11,7 +11,29 @@ function Sidebar() {
   return (
     <div className=' min-h-screen bg-white border-r w-[35%] lg:w-[20%] ' >
         {
-            (aToken || dToken) && <ul className='text-[#515151] mt-5' >
+            aToken && <ul className='text-[#515151] mt-5' >
+                <NavLink className={({isActive})=>`flex items-center gap-3 px-3 py-3.5 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4 border-primary':''} `} to={'/admin-dashboard'}>
+                    <img src={assets.home_icon} alt="" />
+                    <p className='hidden md:block'>Dashboard</p>
+                </NavLink>
+                <NavLink className={({isActive})=>`flex items-center gap-3 px-3 py-3.5 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4 border-primary':''} `}   to={'/all-appointments'}>
+                    <img src={assets.appointment_icon} alt="" />
+                    <p  className='hidden md:block'>Appointments</p>
+                </NavLink>
+               
+                <NavLink className={({isActive})=>`flex items-center gap-3 px-3 py-3.5 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4 border-primary':''} `} to={'/add-doctor'}>
+                    <img src={assets.add_icon} alt="" />
+                    <p  className='hidden md:block'>Add Doctor</p>
+                </NavLink>
+
+                <NavLink className={({isActive})=>`flex items-center gap-3 px-3 py-3.5 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4 border-primary':''} `} to={'/doctor-list'}>
+                    <img src={assets.people_icon} alt="" />
+                    <p  className='hidden md:block'>Doctors List</p>
+                </NavLink>
+            </ul>
+        }
+        {
+            dToken && <ul className='text-[#515151] mt-5' >
                 <NavLink className={({isActive})=>`flex items-center gap-3 px-3 py-3.5 md:px-9 md:min-w-72 cursor-pointer ${isActive?'bg-[#F2F3FF] border-r-4 border-primary':''} `} to={'/doctor-dashboard'}>
                     <img src={assets.home_icon} alt="" />
                     <p className='hidden md:block'>Dashboard</p>
