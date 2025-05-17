029195c1ec2b4346dc687b1a4fcc2a67dc0e2d2d
Ishank <deshwalishank@gmail.com>
2025-05-18 04:24:45 +0530

    done

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -57,6 +57,7 @@ function Navbar() {
                     </div>
                 </div>
                 :<button onClick={()=> navigate('/login') } className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block '>Create Account</button>
+
             }
             <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} alt="" />
             {/* Mobile menu */}
